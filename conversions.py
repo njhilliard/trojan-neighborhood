0d56d632f456d13e20c68fb21a91c441e8fc8229
Nate Hilliard <natejhilliard@gmail.com>
2019-11-04 14:05:20 -0600

    fixing more whitespace

--- conversions.py
+++ conversions.py
@@ -29,75 +29,75 @@ def GetTempAdiabatic(u, gamma):
         return (gamma-1.)*u/Boltzmann*UnitEnergy_in_cgs/UnitMass_in_g * MeanWeight
 
 def GetTemp(u, Nelec, gamma):
-	"""
-	RETURNS: temperature in Kelvin
-	INPUT: u     : thermal energy
-	       Nelec : electron abundance
-	       gamma : adiabatic index	
-    	"""
-	MeanWeight = 4./(1.+3.*Hydrogen_MassFrac+4.*Hydrogen_MassFrac*Nelec) * ProtonMass
-	return (gamma-1.)*u/Boltzmann*UnitEnergy_in_cgs/UnitMass_in_g * MeanWeight
+        """
+        RETURNS: temperature in Kelvin
+        INPUT: u     : thermal energy
+               Nelec : electron abundance
+               gamma : adiabatic index  
+        """
+        MeanWeight = 4./(1.+3.*Hydrogen_MassFrac+4.*Hydrogen_MassFrac*Nelec) * ProtonMass
+        return (gamma-1.)*u/Boltzmann*UnitEnergy_in_cgs/UnitMass_in_g * MeanWeight
 
 def GetPressure(u, rho, gamma):
-	"""
-	RETURNS: pressure in simulation units
-	INPUT: u     : thermal energy
-	       rho   : density
-	       gamma : adiabatic index	
-    	"""
-	return (gamma-1.)*rho*u
+        """
+        RETURNS: pressure in simulation units
+        INPUT: u     : thermal energy
+               rho   : density
+               gamma : adiabatic index  
+        """
+        return (gamma-1.)*rho*u
 
 def GetEntropy(u, rho, gamma):
-	"""
-	RETURNS: entropic function in simulation units
-	INPUT: u     : thermal energy
-	       rho   : density
-	       gamma : adiabatic index	
-    	"""
-	return GetPressure(u, rho, gamma) / rho**gamma
-	
+        """
+        RETURNS: entropic function in simulation units
+        INPUT: u     : thermal energy
+               rho   : density
+               gamma : adiabatic index  
+        """
+        return GetPressure(u, rho, gamma) / rho**gamma
+        
 def GetRhoCrit():
-	"""
-	RETURNS: critical density
-    	"""
-	return 3.*Hubble**2./(8.*pi*G)
+        """
+        RETURNS: critical density
+        """
+        return 3.*Hubble**2./(8.*pi*G)
 
 def GetnH(rho, ascale, h=0.7):
-	"""
-	RETURNS: physical hydrogen number density in h^2 cm^-3
-	INPUT: rho    : density
-	       ascale : scale factor	
-	       h      : Hubble constant
-    	"""
-	return (rho*UnitDensity_in_cgs*h**2.) * (Hydrogen_MassFrac/ProtonMass) / ascale**3.
+        """
+        RETURNS: physical hydrogen number density in h^2 cm^-3
+        INPUT: rho    : density
+               ascale : scale factor    
+               h      : Hubble constant
+        """
+        return (rho*UnitDensity_in_cgs*h**2.) * (Hydrogen_MassFrac/ProtonMass) / ascale**3.
 
 def GetTime(ascale, OmegaM=0.27,OmegaL=0.73,h=0.7):
-	"""
-	RETURNS: time for given cosmology and scale factor in simulation units
-	INPUT: ascale : scale factor
-	       OmegaM : Omega Matter
-	       OmegaL : Omega Lambda
-	       h      : Hubble constant
-    	"""
-	aml=(OmegaM/OmegaL)**(1./3.)
-	return 1./(h*Hubble) * 2./(3.* (1.-OmegaM)**0.5) * np.log((ascale/aml)**1.5 + (1.+(ascale/aml)**3.)**0.5) * UnitTime_in_Gyr
+        """
+        RETURNS: time for given cosmology and scale factor in simulation units
+        INPUT: ascale : scale factor
+               OmegaM : Omega Matter
+               OmegaL : Omega Lambda
+               h      : Hubble constant
+        """
+        aml=(OmegaM/OmegaL)**(1./3.)
+        return 1./(h*Hubble) * 2./(3.* (1.-OmegaM)**0.5) * np.log((ascale/aml)**1.5 + (1.+(ascale/aml)**3.)**0.5) * UnitTime_in_Gyr
 
 def GetLookBackTime(ascale, OmegaM=0.27,OmegaL=0.73,h=0.7):
-	"""
-	RETURNS: lookback time in simulation units
-	INPUT: ascale : scale factor
-	       OmegaM : Omega Matter
-	       OmegaL : Omega Lambda
-	       h      : Hubble constant
-    	"""
+        """
+        RETURNS: lookback time in simulation units
+        INPUT: ascale : scale factor
+               OmegaM : Omega Matter
+               OmegaL : Omega Lambda
+               h      : Hubble constant
+        """
 
-	return GetTime(1.,OmegaM, OmegaL, h) - GetTime(ascale,OmegaM, OmegaL, h)
-	
+        return GetTime(1.,OmegaM, OmegaL, h) - GetTime(ascale,OmegaM, OmegaL, h)
+        
 def GetCoolingRate(coor, rho):
         """
         RETURNS: cooling rate in erg s^-1 cm^-3 h^3  
         INPUT: coor : cooling rate in code units
-	       rho : density in code units 
+               rho : density in code units 
         """
 
-	return coor*rho * UnitEnergy_in_cgs *  UnitTime_in_s**(-1.0) * UnitLength_in_cm**(-3.0) 	
+        return coor*rho * UnitEnergy_in_cgs *  UnitTime_in_s**(-1.0) * UnitLength_in_cm**(-3.0)         
