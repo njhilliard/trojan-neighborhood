def919688a4f60a1197339848fcc40a457bcfed9
Nate Hilliard <natejhilliard@gmail.com>
2019-11-04 14:04:19 -0600

    fixing whitespace in plot script, scp'd conversions

--- plot_MW_hdf5.py
+++ plot_MW_hdf5.py
@@ -80,7 +80,7 @@ rmax=15.0
 #bin points
 dtheta=2*np.pi/BINS_theta
 dr=(rmax-rmin)/BINS_r
-thetamid=(np.arange(BINS_theta)+0.5) * dtheta - np.pi	
+thetamid=(np.arange(BINS_theta)+0.5) * dtheta - np.pi   
 rmid=(np.arange(BINS_r)+0.5) * dr + rmin
 
 
@@ -107,52 +107,52 @@ py=np.zeros(15000000)
 pz=np.zeros(15000000)
 
 for i in range(0,len(nums)):
-	num=nums[i]
+        num=nums[i]
 
-	#####commented: how to read in hdf5
-	filename=base+snapbase+str(num).zfill(3)+".hdf5"	
-	#filename=base+"snapdir_"+str(num).zfill(3)+"/"+snapbase+str(num).zfill(3)
-        print( filename )
-	with h5py.File(filename,"r") as filename:
-	#with h5py.File("/home/donghia/MW_model_x20_f20_VC16hd_new_snapshot_306.hdf5","r") as filename:
+        #####commented: how to read in hdf5
+        filename=base+snapbase+str(num).zfill(3)+".hdf5"        
+        #filename=base+"snapdir_"+str(num).zfill(3)+"/"+snapbase+str(num).zfill(3)
+        print(filename)
+        with h5py.File(filename,"r") as filename:
+        #with h5py.File("/home/donghia/MW_model_x20_f20_VC16hd_new_snapshot_306.hdf5","r") as filename:
 
-		###this indented block must be done to access all data inside the hdf5 file opened as 'filename'
-		myHead = filename.__getitem__("/Header")
-		pDisk = filename.__getitem__("/PartType2")
+                ###this indented block must be done to access all data inside the hdf5 file opened as 'filename'
+                myHead = filename.__getitem__("/Header")
+                pDisk = filename.__getitem__("/PartType2")
                 pBulge = filename.__getitem__("/PartType3")
-		diskCoord = np.array(pDisk.__getitem__("Coordinates"))
-		diskVels = np.array(pDisk.__getitem__("Velocities"))
+                diskCoord = np.array(pDisk.__getitem__("Coordinates"))
+                diskVels = np.array(pDisk.__getitem__("Velocities"))
                 bulgeCoord = np.array(pBulge.__getitem__("Coordinates"))
                 bulgeVels = np.array(pBulge.__getitem__("Velocities"))
-		####your coords and vels are arrays of size (nParticles_disk,3)
-		att = myHead.attrs  ###your header attributes
-		myTime = att.__getitem__("Time")
-		myMasses = np.array(att.__getitem__("MassTable"))
-		myDiskMass = myMasses[2]
+                ####your coords and vels are arrays of size (nParticles_disk,3)
+                att = myHead.attrs  ###your header attributes
+                myTime = att.__getitem__("Time")
+                myMasses = np.array(att.__getitem__("MassTable"))
+                myDiskMass = myMasses[2]
                 myBulgeMass = myMasses[3] 
-		print( 'Processing ', filename		)
+                print( 'Processing ', filename          )
 
-	###here after the indentation of the 'with' block is done, python will close the hdf5 file correctly and safe
+        ###here after the indentation of the 'with' block is done, python will close the hdf5 file correctly and safe
 
-	#head=rs.snapshot_header(filename)
-	#print( "time = ", head.time)
-	print( "time = ", myTime)
-	print( "length of coordinate of disk is ", diskCoord.shape)
+        #head=rs.snapshot_header(filename)
+        #print( "time = ", head.time)
+        print( "time = ", myTime)
+        print( "length of coordinate of disk is ", diskCoord.shape)
         print( "length of coordinate of bulge is ", bulgeCoord.shape)
-	print( "myDiskMass is ", myMasses[2], myMasses[3])
+        print( "myDiskMass is ", myMasses[2], myMasses[3])
 
 
-	#pos=rs.read_block(filename, "POS ", parttype=2).astype('float64')
+        #pos=rs.read_block(filename, "POS ", parttype=2).astype('float64')
         #poss=rs.read_block(filename, "POS ", parttype=2).astype('float64')
         #vel=rs.read_block(filename, "VEL ", parttype=2).astype('float64')
         ##pos=rs.read_block(filename, "POS ", parttype=3).astype('float64')
         #mass=rs.read_block(filename, "MASS", parttype=2).astype('float64')
         ##mass=rs.read_block(filename, "MASS", parttype=3).astype('float64')
-	   
+           
 
         #px=pos[:,xax]
-        #py=pos[:,yax]	
-	#pz=pos[:,zax]
+        #py=pos[:,yax]  
+        #pz=pos[:,zax]
 
         px = diskCoord[:,xax]
         py = diskCoord[:,yax] 
@@ -163,7 +163,7 @@ for i in range(0,len(nums)):
         pz = np.append(pz, bulgeCoord[:,zax])
        
         
-	#xcm = px.sum()/len(px)
+        #xcm = px.sum()/len(px)
         #ycm = py.sum()/len(py)
         #zcm = pz.sum()/len(pz)
         xcm = np.median(px)
@@ -174,7 +174,7 @@ for i in range(0,len(nums)):
 
         px = px - xcm
         py = py - ycm
-	pz = pz - zcm
+        pz = pz - zcm
 
         #xnew = px.sum()/len(px)
         #ynew = py.sum()/len(py)
@@ -196,8 +196,8 @@ for i in range(0,len(nums)):
 
         #print( "min/max of log10(histogram) = ", pmin, pmax)
         #vx=vel[:,xax]
-        #vy=vel[:,yax]	
-	#vz=vel[:,zax]
+        #vy=vel[:,yax]  
+        #vz=vel[:,zax]
 
         
         vx = diskVels[:,xax]
@@ -218,58 +218,58 @@ for i in range(0,len(nums)):
 
         h, x, y = np.histogram2d(r,theta,bins=[BINS_r,BINS_theta],range=[[rmin,rmax],[-np.pi,np.pi]])
 
-	#divide by area to get surface density
-	for i in range(0,BINS_r):
-		h[i,:]/=rmid[i]*dr*dtheta
+        #divide by area to get surface density
+        for i in range(0,BINS_r):
+                h[i,:]/=rmid[i]*dr*dtheta
 
-	#fit the axisymmetric surface density		 
-	meanh=np.zeros(BINS_r)
-	for i in range(0,BINS_r):
-		meanh[i]=h[i,:].mean()
+        #fit the axisymmetric surface density            
+        meanh=np.zeros(BINS_r)
+        for i in range(0,BINS_r):
+                meanh[i]=h[i,:].mean()
         z=np.polyfit(rmid, np.log(meanh), 1)
-	Rs=-1/z[0]
-	p = np.poly1d(z)
-	print( "Rs = ", Rs, mass )
+        Rs=-1/z[0]
+        p = np.poly1d(z)
+        print( "Rs = ", Rs, mass )
 
-	#calculate residuals
-	for i in range(0,BINS_r):
-		#h[i,:]=(h[i,:] - np.exp(p(rmid[i]))) / np.exp(p(rmid[i]))
-		h[i,:]=(h[i,:] - h[i,:].mean()) / (h[i,:].mean())
+        #calculate residuals
+        for i in range(0,BINS_r):
+                #h[i,:]=(h[i,:] - np.exp(p(rmid[i]))) / np.exp(p(rmid[i]))
+                h[i,:]=(h[i,:] - h[i,:].mean()) / (h[i,:].mean())
         
         #print( mass.dtype)
         #print( "xxxxxx", mass.shape, px.shape)
-	#construct mass-weighted histograms
-	#Z,x,y=np.histogram2d(px/Rs,py/Rs, range=[[-lengthX,lengthX],[-lengthY,lengthY]], weights=mass, bins=BINS, normed=True)
-	Z,x,y=np.histogram2d(px/Rs,py/Rs, range=[[-lengthX/Rs,lengthX/Rs],[-lengthY/Rs,lengthY/Rs]], bins=BINS, normed=True)
+        #construct mass-weighted histograms
+        #Z,x,y=np.histogram2d(px/Rs,py/Rs, range=[[-lengthX,lengthX],[-lengthY,lengthY]], weights=mass, bins=BINS, normed=True)
+        Z,x,y=np.histogram2d(px/Rs,py/Rs, range=[[-lengthX/Rs,lengthX/Rs],[-lengthY/Rs,lengthY/Rs]], bins=BINS, normed=True)
       
-	Z=np.log10(Z)
+        Z=np.log10(Z)
         
    
-	Zmin=Z[Z>-np.inf].min()
-	Zmax=Z[Z<np.inf].max()
-	if ((Zmax==0.0) & (Zmin==0.0)):
-		Zmin=Z[Z>-np.inf].min()
-		Zmax=Z.max()
-	else:
-		Z[Z<Zmin]=Zmin
-		Z[Z>Zmax]=Zmax
+        Zmin=Z[Z>-np.inf].min()
+        Zmax=Z[Z<np.inf].max()
+        if ((Zmax==0.0) & (Zmin==0.0)):
+                Zmin=Z[Z>-np.inf].min()
+                Zmax=Z.max()
+        else:
+                Z[Z<Zmin]=Zmin
+                Z[Z>Zmax]=Zmax
 
-	print( "min/max of log10(histogram) = ", Zmin, Zmax)
+        print( "min/max of log10(histogram) = ", Zmin, Zmax)
 
 
 
-	fig = plt.figure(1, figsize=(25.0,25.0))
-	
-	#left plot
-	#ax = fig.add_subplot(1,2,1,title=tname+"  t="+str(round(head.time*co.UnitTime_in_Gyr*1000.0,1))+"Myr")
+        fig = plt.figure(1, figsize=(25.0,25.0))
+        
+        #left plot
+        #ax = fig.add_subplot(1,2,1,title=tname+"  t="+str(round(head.time*co.UnitTime_in_Gyr*1000.0,1))+"Myr")
         
         ax = fig.add_subplot(1,2,1) #,title=tname+"  t="+str(round(myTime*co.UnitTime_in_Gyr*1000.0,1))+"Myr")
-	im=ax.imshow(Z.T, vmin=Zmin, vmax=Zmax, origin='lower',interpolation='nearest', extent=[-lengthX/Rs,lengthX/Rs,-lengthY/Rs,lengthY/Rs], cmap=cm.get_cmap('jet'))
+        im=ax.imshow(Z.T, vmin=Zmin, vmax=Zmax, origin='lower',interpolation='nearest', extent=[-lengthX/Rs,lengthX/Rs,-lengthY/Rs,lengthY/Rs], cmap=cm.get_cmap('jet'))
         ax.set_xlabel('x/Rs', fontsize=18, fontweight='bold')
         ax.set_ylabel('y/Rs',fontsize=18, fontweight='bold')
         plt.xticks(np.arange(-round(lengthX/Rs), round(lengthX/Rs), step=2), fontsize=15, fontweight='bold')
         plt.yticks(np.arange(-round(lengthY/Rs), round(lengthY/Rs), step=2), fontsize=15, fontweight='bold')
-	plt.colorbar(im, shrink=0.35)        
+        plt.colorbar(im, shrink=0.35)        
 
 #  I DEFINE THE PATCH
         ##rlow=6.0
@@ -282,17 +282,17 @@ for i in range(0,len(nums)):
        # kindex=(r>8.0) & (r<8.2) & (theta>2.45) & (theta<5.5)  
        # print( "UFF", len(kindex))
        # urnew=ur[kindex]-vx0
-	# vtnew=vt[kindex]-vy0
+        # vtnew=vt[kindex]-vy0
         ##im=ax.imshow(h.T, vmin=-0.75, vmax=0.75, origin='lower',interpolation='nearest',extent=[-300, 300, -300, 300], cmap=cm.get_cmap('jet'))
         # ax.scatter(urnew,vtnew,s=patch_s,c='w',facecolors='black',edgecolors='black')
-	# if (head.npart[2] > 0) & (patch_flag):
-	#	urnew=ur[kindex]-vx0
+        # if (head.npart[2] > 0) & (patch_flag):
+        #       urnew=ur[kindex]-vx0
         #        vtnew=vt[kindex]-vy0
-	#	ax.scatter(urnew,vtnew,s=patch_s,c='w',facecolors='black',edgecolors='black')
+        #       ax.scatter(urnew,vtnew,s=patch_s,c='w',facecolors='black',edgecolors='black')
   
 
 
-#	if (head.npart[2] > 0) & (mc_flag):
+#       if (head.npart[2] > 0) & (mc_flag):
 #                if (r.all < rhigh) & (r.all > rlow) & (theta.all < thetahigh) & (theta.all > thetalow):
 #                    print( r,theta )
 #                    poss=rs.read_block(filename, "POS ", parttype=2, arepo=arepo).astype('float64')
@@ -301,18 +301,18 @@ for i in range(0,len(nums)):
 #                    ax.scatter(pxx/rh,pyy/rh,s=mc_s,c='w',facecolors='black',edgecolors='black')
 #                    ax.axis([-lengthX,lengthX,-lengthY,lengthY])
                         #ax.scatter(ur,vt,s=mc_s,c='w',facecolors='black',edgecolors='black') 
-	        ##pos=rs.read_block(filename, "POS ", parttype=3, arepo=arepo).astype('float64')
-		#poss=rs.read_block(filename, "POS ", parttype=3).astype('float64')
-       		#pxx=poss[:,xax]
-	        #pyy=poss[:,yax]
-        	#ax.scatter(pxx/rh,pyy/rh,s=mc_s,c='w',facecolors='black',edgecolors='black')
-	        #ax.axis([-lengthX,lengthX,-lengthY,lengthY])
-	##if counter==len(nums1)-1:
+                ##pos=rs.read_block(filename, "POS ", parttype=3, arepo=arepo).astype('float64')
+                #poss=rs.read_block(filename, "POS ", parttype=3).astype('float64')
+                #pxx=poss[:,xax]
+                #pyy=poss[:,yax]
+                #ax.scatter(pxx/rh,pyy/rh,s=mc_s,c='w',facecolors='black',edgecolors='black')
+                #ax.axis([-lengthX,lengthX,-lengthY,lengthY])
+        ##if counter==len(nums1)-1:
         #print( 'nnn', len(urnew))
 
 # commento da Tenerife
-	# ax.set_xlabel('u', fontsize=18)
-	# ax.set_ylabel('v', fontsize=18)
+        # ax.set_xlabel('u', fontsize=18)
+        # ax.set_ylabel('v', fontsize=18)
 # fine commento
 
         #print( 'cccc', pxx,pyy )
@@ -322,7 +322,7 @@ for i in range(0,len(nums)):
         #add on top particle type 3
         #if (head.npart[4] > 0) & (mc_flag):
                 ##pos=rs.read_block(filename, "POS ", parttype=3, arepo=arepo).astype('float64')
-		#pos=rs.read_block(filename, "POS ", parttype=3).astype('float64')
+                #pos=rs.read_block(filename, "POS ", parttype=3).astype('float64')
                 #px=pos[:,xax]
                 #py=pos[:,yax]
                 #r=np.sqrt(px**2. + py**2.)
@@ -337,11 +337,11 @@ for i in range(0,len(nums)):
         cbar2=plt.colorbar(im2, shrink=0.35)
         cbar2.set_label("$(\Sigma - \Sigma_{\mathrm{fit}}) / \Sigma_{\mathrm{fit}} $", fontsize=18, fontweight='bold')
 
-	#plt.colorbar(im, shrink=0.5)
-###	plt.savefig(output_dir+"uvmultipatch_8_150_"+str(num).zfill(3)+".png", dpi=100, bbox_inches='tight')
- 	plt.savefig(output_dir+"M4disk_new"+str(num).zfill(3)+".png", dpi=100, bbox_inches='tight')
+        #plt.colorbar(im, shrink=0.5)
+###     plt.savefig(output_dir+"uvmultipatch_8_150_"+str(num).zfill(3)+".png", dpi=100, bbox_inches='tight')
+        plt.savefig(output_dir+"M4disk_new"+str(num).zfill(3)+".png", dpi=100, bbox_inches='tight')
         #plt.savefig("bar306.pdf") 
-	fig.clf()
+        fig.clf()
 
 
 
